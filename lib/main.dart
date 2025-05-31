aa5cbedf0b1d82d7c89346160d12b19598bab340
Gustavohaa <111452073+Gustavohaa@users.noreply.github.com>
2025-05-30 21:05:43 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,243 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Country {
+  final String name;
+  final String flag;
+  final String capital;
+  final int population;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Country({
+    required this.name,
+    required this.flag,
+    required this.capital,
+    required this.population,
+  });
+
+  factory Country.fromJson(Map<String, dynamic> json) {
+    return Country(
+      name: json['name']['common'],
+      flag: json['flags']['png'],
+      capital: (json['capital'] != null && json['capital'].isNotEmpty)
+          ? json['capital'][0]
+          : 'Sem capital',
+      population: json['population'],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lazy Loading de Países',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const CountryListPage(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CountryListPage extends StatefulWidget {
+  const CountryListPage({super.key});
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  State<CountryListPage> createState() => _CountryListPageState();
+}
+
+class _CountryListPageState extends State<CountryListPage> {
+  final ScrollController _scrollController = ScrollController();
+  List<Country> _allCountries = [];
+  List<Country> _visibleCountries = [];
+  int _currentMax = 10;
+  bool _isLoading = false;
+  bool _hasError = false;
+
+  Future<void> _fetchCountries() async {
+    try {
+      final response = await http.get(Uri.parse('https://restcountries.com/v3.1/all'));
+
+      if (response.statusCode == 200) {
+        List data = json.decode(response.body);
+        data.sort((a, b) => a['name']['common'].compareTo(b['name']['common']));
+        setState(() {
+          _allCountries = data.map((json) => Country.fromJson(json)).toList();
+          _visibleCountries = _allCountries.take(_currentMax).toList();
+          _hasError = false;
+        });
+      } else {
+        setState(() {
+          _hasError = true;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Erro ao carregar países: ${response.statusCode}')),
+        );
+      }
+    } catch (e) {
+      setState(() {
+        _hasError = true;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro de conexão: $e')),
+      );
+    }
+  }
+
+  void _loadMore() {
+    if (_isLoading || _visibleCountries.length >= _allCountries.length) return;
+
+    setState(() => _isLoading = true);
+
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _currentMax += 10;
+        _visibleCountries = _allCountries.take(_currentMax).toList();
+        _isLoading = false;
+      });
+    });
+  }
+
+  void _checkIfNeedsMoreData() {
+    if (_scrollController.position.maxScrollExtent == 0 &&
+        _visibleCountries.length < _allCountries.length) {
+      _loadMore();
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _checkIfNeedsMoreData();
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCountries().then((_) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _checkIfNeedsMoreData();
+      });
+    });
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.atEdge &&
+          _scrollController.position.pixels != 0) {
+        _loadMore();
+      }
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _showDetails(Country country) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CountryDetailPage(country: country),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    );
+  }
+
+  Widget _buildProgressIndicator() {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Lista de Países')),
+      body: SafeArea(
+        child: _hasError
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Erro ao carregar dados'),
+                    ElevatedButton(
+                      onPressed: _fetchCountries,
+                      child: const Text('Tentar novamente'),
+                    ),
+                  ],
+                ),
+              )
+            : _allCountries.isEmpty
+                ? _buildProgressIndicator()
+                : ListView.builder(
+                    controller: _scrollController,
+                    itemCount: _visibleCountries.length + (_isLoading ? 1 : 0),
+                    itemBuilder: (context, index) {
+                      if (index >= _visibleCountries.length) {
+                        return _buildProgressIndicator();
+                      }
+                      final country = _visibleCountries[index];
+                      return ListTile(
+                        leading: Image.network(
+                          country.flag,
+                          width: 50,
+                          height: 30,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              const Icon(Icons.flag),
+                        ),
+                        title: Text(country.name),
+                        subtitle: Text('Capital: ${country.capital}'),
+                        onTap: () => _showDetails(country),
+                      );
+                    },
+                  ),
+      ),
+    );
+  }
+}
+
+class CountryDetailPage extends StatelessWidget {
+  final Country country;
+
+  const CountryDetailPage({super.key, required this.country});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(country.name)),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.network(
+              country.flag,
+              height: 150,
+              errorBuilder: (context, error, stackTrace) =>
+                  const Icon(Icons.flag, size: 150),
+            ),
+            const SizedBox(height: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Capital: ${country.capital}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'População: ${country.population.toString().replaceAllMapped(
+                    RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+                    (Match m) => '${m[1]}.',
+                  )}',
+              style: const TextStyle(fontSize: 18),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
